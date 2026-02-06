cc73be29780762c4e3015e803fc41e25fc70cac1
Samarth Sanjay Pyati <samarthpyati@gmail.com>
2026-02-06 11:13:28 +0530

    Add LOG_ERROR and updated other functions.

--- src/lexer.h
+++ src/lexer.h
@@ -1,3 +1,8 @@
+#ifndef LEXER_H_
+#define LEXER_H_
+
+#include <sv_str.h>
+
 /* Minimal lexer logic
 
 while not EOF:
@@ -8,7 +13,39 @@ while not EOF:
     if symbol → read symbol token
 */
 
-#ifndef LEXER_H_
-#define LEXER_H_
+typedef enum {
+    // Single-character tokens
+    LEFT_PAREN = 0, 
+    RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    // End of file
+    EOF, 
+
+    TOKEN_TYPE_COUNT
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    StringView lexeme;    
+    void *literal;
+
+    // Error reporting
+    const char *file;
+    size_t line;
+} Token;
 
 #endif // LEXER_H_
--- src/utils.h
+++ src/utils.h
@@ -1,20 +1,32 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
-#define Stmt(s) do { S } while (0)
-#define UNUSED(x) (void)(x)
+#include <stdio.h>
+#include <stdlib.h>
 
-#define UNIMPLEMENTED(msg) Stmt (                                                           \
-    fprintf(stderr, "%s:%d: [%s UNIMPLEMENTED] : %s\n", __FILE__, __LINE__, __func__, msg); \
-    abort();                                                                                \
+#define STMT(S) do { S } while (0)
+
+#define UNUSED(value) (void)(value)
+#define NOB_TODO(message) STMT (                                            \
+    fprintf(stderr, "%s:%d: TODO: %s\n", __FILE__, __LINE__, message);      \
+    abort();                                                                \
+)                                                                           
+#define UNIMPLEMENTED(message) STMT (                                           \
+    fprintf(stderr, "%s:%d: UNIMPLEMENTED: %s\n", __FILE__, __LINE__, message); \
+    abort();                                                                    \
 )
-#define UNREACHABLE(msg) Stmt (                                                             \
-    fprintf(stderr, "%s:%d: [UNREACHABLE] : %s\n", __FILE__, __LINE__, msg);                \
-    abort();                                                                                \
+#define UNREACHABLE(message) STMT (                                                 \
+    fprintf(stderr, "%s:%d: UNREACHABLE : %s\n", __FILE__, __LINE__, message);      \
+    abort();                                                                        \
 )
 
 #define ArrayLen(xs) sizeof((xs)) / sizeof(*(xs))
 
 #define KB(b) (uint64_t)(b) << 10
+
+/* Error handling */
+#define LOG_ERROR(fmt, ...) STMT (                                                \
+    fprintf(stderr, "%s:%d: ERROR: "fmt"", __FILE__, __LINE__, ##__VA_ARGS__);    \
+)
 
 #endif // UTILS_H_
